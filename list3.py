95f0da88708018926b47e0dd03104a6ec6708b3f
Arpita Tiwari <arpitatevari@gmail.com>
2023-04-07 20:36:05 +0530

    list and string

--- list3.py
+++ list3.py
@@ -1,5 +1,5 @@
 #Write a program that creates a list of strings and then sorts the list alphabetically. Print the sorted list.
-a=["Arpita", "Mannat","Sudipto","Shank","Ekta"]
+a=["Arpitaa", "Mannat","Sudipto","Shank","Ekta"]
 b=sorted(a)
 print(a)
 print(b)
