b0e5976c979793f14c7b5cbc4cee044af474e8de
Arpita Tiwari <arpitatevari@gmail.com>
2023-04-17 12:22:02 +0530

    leet

--- list6.py
+++ list6.py
@@ -1,4 +1,4 @@
-#!!!!!Write a program that creates a list of strings and then removes all elements that contain a specific substring. 
+#Write a program that creates a list of strings and then removes all elements that contain a specific substring. 
 #Print the final list.
 a=["Arpita", "Mannat","Sudipto","Shank","Ekta"]
 print(a)
--- list9.py
+++ list9.py
@@ -1,4 +1,4 @@
-#!!!!Write a program that creates a list of strings and then finds the longest string in the list. 
+#Write a program that creates a list of strings and then finds the longest string in the list. 
 #Print the longest string.
 a=["Arpita", "Mannat","Sudipto","Shashank","Ekta"]
 print(a)
